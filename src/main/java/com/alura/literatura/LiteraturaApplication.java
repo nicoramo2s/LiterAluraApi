db06e873e03e6f2e8675a10957df60f7f36a1d52
nicoramo2s <nicolasramo2s@hotmail.com>
2024-06-01 00:30:32 -0300

    completo hasta listar autores

--- src/main/java/com/alura/literatura/LiteraturaApplication.java
+++ src/main/java/com/alura/literatura/LiteraturaApplication.java
@@ -1,13 +1,31 @@
 package com.alura.literatura;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.alura.literatura.principal.Principal;
+import com.alura.literatura.repository.AutoresRepository;
+import com.alura.literatura.repository.LibrosRepository;
+
 @SpringBootApplication
-public class LiteraturaApplication {
+public class LiteraturaApplication implements CommandLineRunner {
+
+	@Autowired
+	private LibrosRepository librosRepository;
+
+	@Autowired
+	private AutoresRepository autoresRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiteraturaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal(librosRepository, autoresRepository);
+		principal.muestraElMenu();
 	}
 
 }
